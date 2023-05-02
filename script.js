a00a96c4507e775a890bc7c1d1e1476dc16febbf
Enzo Sabatino <enzosabatino@hotmail.com>
2023-05-02 14:44:07 -0300

    segunda entrega

---
+++ script.js
@@ -0,0 +1,29 @@
+const bachas = [
+  { tipo: "simple", precio: 20000 },
+  { tipo: "doble", precio: 40000 },
+  { tipo: "cuadrada", precio: 60000 },
+  { tipo: "circular", precio: 15000 }
+];
+
+let seguirComprando = true;
+let total = 0;
+
+while (seguirComprando) {
+  const tipoBacha = prompt(`Elija el tipo de bacha que desea comprar: ${bachas.map(bacha => bacha.tipo).join(', ')}`);
+  const cantidadBachas = parseInt(prompt('Ingrese la cantidad de bachas que desea comprar: '));
+  const bachaElegida = bachas.find(bacha => bacha.tipo === tipoBacha);
+
+  if (bachaElegida) {
+    const precioTotal = bachaElegida.precio * cantidadBachas;
+    total += precioTotal;
+    alert(`Usted ha comprado ${cantidadBachas} bachas ${tipoBacha}, con un precio total de $${precioTotal.toLocaleString('es-AR')}. El total a pagar hasta ahora es de $${total.toLocaleString('es-AR')}.`);
+
+  } else {
+    alert('La bacha elegida no existe. Por favor, elija otra.');
+  }
+
+  const seguirComprandoStr = prompt('¿Desea seguir comprando bachas? (s/n)');
+  seguirComprando = seguirComprandoStr.toLowerCase() === 's';
+}
+
+alert(`El total a pagar es de$ $${total.toLocaleString('es-AR')}. Gracias por su compra!`);
